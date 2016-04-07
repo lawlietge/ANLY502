4aeeb0f08b76a4ae3ec819eee205dbef1f24b4a3
Simson Garfinkel <simsong@acm.org>
2016-04-06 20:54:33 -0400

    cleaned

--- PS05/agents_cleanup.py
+++ PS05/agents_cleanup.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python2.7
 
 """
-Read the file agents.raw and output agents.csv, a comma-separated file
+Read the file agents.raw and output agents.txt, a readable file
 """
 
 if __name__=="__main__":
-    with open("agents.txt","r") as f:
+    with open("agents.raw","r") as f:
         with open("agents.txt","w") as out:
             for line in f:
                 if(line[0] in '+|'):
